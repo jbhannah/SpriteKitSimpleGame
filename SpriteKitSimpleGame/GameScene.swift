fbbe3530268fc806b242bbd8a9e8e82b520545f8
Jesse B. Hannah <jesse@jbhannah.net>
2016-06-22 04:15:42 -0700

    Add monster to scene

--- SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/GameScene.swift
@@ -20,5 +20,44 @@ class GameScene: SKScene {
 
         // Add the player to the scene
         addChild(player)
+
+        // Spawn monsters
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addMonster),
+                SKAction.waitForDuration(1.0)
+            ])
+        ))
+    }
+
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+
+    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+
+    func addMonster() {
+        // Create sprite
+        let monster = SKSpriteNode(imageNamed: "monster")
+
+        // Determine where to spawn the monster along the y axis
+        let actualY = random(min: monster.size.height / 2, max: size.height - monster.size.height / 2)
+
+        // Position the monster slightly off-screen along the right edge,
+        // and at a random position along the Y axis
+        monster.position = CGPoint(x: size.width + monster.size.width / 2, y: actualY)
+
+        // Add the monster to the scene
+        addChild(monster)
+
+        // Determine the speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+
+        // Create the actions
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width / 2, y: actualY), duration: NSTimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
     }
 }
