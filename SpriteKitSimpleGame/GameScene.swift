bd11f352b0873e51ae2261ad7507c320a22c6dae
Jesse B. Hannah <jesse@jbhannah.net>
2016-06-22 04:04:45 -0700

    Add player to scene

--- SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/GameScene.swift
@@ -9,37 +9,16 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    // Create sprite
+    let player = SKSpriteNode(imageNamed: "player")
+
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        backgroundColor = SKColor.whiteColor()
+
+        // Determine where to spawn the player
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+
+        // Add the player to the scene
+        addChild(player)
     }
 }
